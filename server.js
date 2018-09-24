9083a1a2f1719215adfd32bd76dc09db6efe7a9e
Nathan Walston <nathanielwalston@gmail.com>
2018-09-24 12:56:14 -0700

    allow for specific colors to be requested

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { Buffer } = require("buffer");
 const Koa = require("koa");
 const app = new Koa();
-const Gif = require('./gif');
+const Gif = require("./gif");
 
 const PORT = process.env.PORT || 9090;
 const DELAY = parseInt(process.env.DELAY) > -1 ? process.env.DELAY : 1000;
@@ -10,9 +10,20 @@ const DELAY = parseInt(process.env.DELAY) > -1 ? process.env.DELAY : 1000;
 app.use(ctx => {
   console.debug(ctx.request.path);
 
+  const color = [rando(), rando(), rando()];
+  const path = /([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})\.gif$/.exec(
+    ctx.request.path || ""
+  );
+  if (path != null) {
+    const [, r, g, b] = path;
+    color[0] = parseInt(r, 16);
+    color[1] = parseInt(g, 16);
+    color[2] = parseInt(b, 16);
+  }
+
   return hold().then(() => {
     ctx.response.headers = { "Content-Type": "image/gif" };
-    ctx.response.body = Gif(rando(), rando(), rando());
+    ctx.response.body = Gif(...color);
   });
 });
 
